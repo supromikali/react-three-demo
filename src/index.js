b36bc884288dd186dc53d81f48d8c744026a9e93
Alexander Solovyev <supromikali@yandex.ru>
2019-04-20 08:08:55 +0400

    start with create-react-app

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+
+class App extends Component {
+    render() {
+        return <div />
+    }
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
