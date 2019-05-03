142d3b19033aad16f3bd1ce76452e12b86d72d1d
Alexander Solovyev <supromikali@yandex.ru>
2019-05-03 22:09:28 +0400

    add comments and wrapper container with mount toggler

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import * as THREE from 'three';
 import {OrbitControls} from "three/examples/jsm/controls/OrbitControls";
 
+const style = {
+    height: 500 // we can control scene size by setting container dimensions
+};
+
 class App extends Component {
     componentDidMount() {
         this.sceneSetup();
@@ -14,25 +18,34 @@ class App extends Component {
     componentWillUnmount() {
         window.removeEventListener('resize', this.handleWindowResize);
         this.stopAnimationLoop();
-        this.removeCustomSceneObjects();
-        this.sceneDestroy();
     }
 
+    // Standard scene setup in Three.js. Check "Creating a scene" manual for more information
+    // https://threejs.org/docs/#manual/en/introduction/Creating-a-scene
     sceneSetup = () => {
+        // get container dimensions and use them for scene sizing
+        const width = this.mount.clientWidth;
+        const height = this.mount.clientHeight;
+
         this.scene = new THREE.Scene();
         this.camera = new THREE.PerspectiveCamera(
-            75,
-            window.innerWidth / window.innerHeight,
-            0.1,
-            1000
+            75, // fov = field of view
+            width / height, // aspect ratio
+            0.1, // near plane
+            1000 // far plane
         );
-        this.camera.position.z = 5;
-        this.controls = new OrbitControls(this.camera);
+        this.camera.position.z = 9; // is used here to set some distance from a cube that is located at z = 0
+        // OrbitControls allow a camera to orbit around the object
+        // https://threejs.org/docs/#examples/controls/OrbitControls
+        this.controls = new OrbitControls(this.camera, this.mount);
         this.renderer = new THREE.WebGLRenderer();
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
-        this.mount.appendChild(this.renderer.domElement);
+        this.renderer.setSize(width, height);
+        this.mount.appendChild(this.renderer.domElement); // mount using React ref
     };
 
+    // Here should come custom code.
+    // Code below is taken from Three.js BoxGeometry example
+    // https://threejs.org/docs/#api/en/geometries/BoxGeometry
     addCustomSceneObjects = () => {
         this.geometry = new THREE.BoxGeometry(2, 2, 2);
         this.material = new THREE.MeshPhongMaterial( {
@@ -58,45 +71,51 @@ class App extends Component {
         this.scene.add( this.lights[ 2 ] );
     };
 
-    animate = () => {
-        this.frameId = requestAnimationFrame(this.animate);
-
+    startAnimationLoop = () => {
         this.cube.rotation.x += 0.01;
         this.cube.rotation.y += 0.01;
 
         this.renderer.render(this.scene, this.camera);
+
+        // The window.requestAnimationFrame() method tells the browser that you wish to perform
+        // an animation and requests that the browser call a specified function
+        // to update an animation before the next repaint
+        this.requestID = requestAnimationFrame(this.startAnimationLoop);
     };
 
-    startAnimationLoop = () => !this.frameId && this.animate();
-
-    stopAnimationLoop = () => {
-        cancelAnimationFrame(this.frameId);
-        this.frameId = null
-    };
-
-    removeCustomSceneObjects = () => {
-        while (this.scene.children.length > 0) {
-            this.scene.remove(this.scene.children[0]);
-        }
-        this.geometry.dispose();
-        this.material.dispose();
-    };
-
-    sceneDestroy = () => {
-        this.mount.removeChild(this.renderer.domElement);
-    };
+    // The window.cancelAnimationFrame() method cancels an animation frame request
+    // previously scheduled through a call to window.requestAnimationFrame()
+    stopAnimationLoop = () => cancelAnimationFrame(this.requestID);
 
     handleWindowResize = () => {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
+        const width = this.mount.clientWidth;
+        const height = this.mount.clientHeight;
+
+        this.renderer.setSize( width, height );
+        this.camera.aspect = width / height;
         this.camera.updateProjectionMatrix();
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-        this.renderer.render( this.scene, this.camera );
     };
 
     render() {
-        return <div ref={ref => (this.mount = ref)} />;
+        return <div style={style} ref={ref => (this.mount = ref)} />;
+    }
+}
+
+class Container extends React.Component {
+    state = {isMounted: true};
+
+    render() {
+        const {isMounted = true} = this.state;
+        return (
+            <>
+                <button onClick={() => this.setState(state => ({isMounted: !state.isMounted}))}>
+                    {isMounted ? "Unmount" : "Mount"}
+                </button>
+                {isMounted && <App />}
+            </>
+        )
     }
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Container />, rootElement);
